0d9de2c16bcf03176a4051a763eb12ab1e6eba43
Anushka <nath.55@osu.edu>
2024-01-10 13:28:17 -0500

    initial commit.

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function HelloWorld() {
-  return <h1 className="greeting">Hello, world!</h1>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    <App />
   </React.StrictMode>
 );
 
